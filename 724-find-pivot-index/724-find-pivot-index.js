4d411910435fbf8fd9c5f76ba18cd3b7c25ea6c9
Jesse Fu <102998153+Jesse132@users.noreply.github.com>
2022-08-25 17:59:22 -0700

    Time: 142 ms (37.02%), Space: 44.1 MB (88.56%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -1,11 +1,12 @@
 var pivotIndex = function(nums) {
+        let left = 0
+        let right = nums.slice().reduce((a, b) => a + b, 0);
+
     for (let i=0; i<nums.length; i++) {
-        let left, right;
         let num = nums[i];
-        i === 0 ? left = 0 : left = nums.slice(0,i).reduce((a, b) => a + b, 0);
-        i === nums.length-1 ? right = 0 : right = nums.slice(i+1,nums.length).reduce((a, b) => a + b, 0);
+        right -= num
         if (left === right) { return i; }
+        left+=nums[i]
     }
     return -1
-    
 };
